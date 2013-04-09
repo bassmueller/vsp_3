87c0cf0f707f1c4ed75a5222b04bab61cfeaba9e
sebastian.mueller <sebastian.mueller@141.22.86.118>
2013-04-09 09:37:25 +0200

    first version

---
+++ VSP_3/src/monitor/Monitor.java
@@ -0,0 +1,59 @@
+package monitor;
+
+import lagern.Lager;
+import lagern.LagerHelper;
+import lagern.MonitorHelper;
+
+import org.omg.CORBA.ORB;
+import org.omg.CORBA.ORBPackage.InvalidName;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.CosNaming.NamingContextPackage.CannotProceed;
+import org.omg.CosNaming.NamingContextPackage.NotFound;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+import org.omg.PortableServer.POAPackage.ServantNotActive;
+import org.omg.PortableServer.POAPackage.WrongPolicy;
+
+public class Monitor {
+	
+	public static void main(String args[]){
+		try{
+			String lagerName = args[4];
+			String monitorName = args[5];
+			ORB orb = ORB.init(args, null);
+			NamingContextExt nc = NamingContextExtHelper.narrow(orb.resolve_initial_references("NameService"));
+			org.omg.CORBA.Object obj = nc.resolve_str(lagerName);
+			Lager lagerRef = LagerHelper.narrow(obj);
+			MonitorImpl monitor = new MonitorImpl(monitorName, orb);
+			POA rootPoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+			//TODO: Muss das gemacht werden?
+			//rootPoa.the_POAManager().activate(); 
+			
+			org.omg.CORBA.Object ref = rootPoa.servant_to_reference(monitor);
+			lagern.Monitor href = MonitorHelper.narrow(ref);
+			lagerRef.monitorHinzufuegen(href);
+			
+			orb.run();
+		}catch (InvalidName e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ServantNotActive e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (WrongPolicy e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (org.omg.CosNaming.NamingContextPackage.InvalidName e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NotFound e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CannotProceed e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ VSP_3/src/monitor/MonitorImpl.java
@@ -0,0 +1,34 @@
+package monitor;
+
+import org.omg.CORBA.ORB;
+
+import lagern.MonitorPOA;
+
+public class MonitorImpl extends MonitorPOA {
+	
+	private String monitorID;
+	private ORB orb;
+	
+	public MonitorImpl(String monitorID, ORB orb){
+		this.monitorID = monitorID;
+		this.orb = orb;
+	}
+	
+	@Override
+	public void meldung(String msg) {
+		System.out.println(String.format("Monitor: %s Log: %s", this.monitorID, msg));
+	}
+
+	//Muss im eigenen Thread aufgerufen werden, da sonst CORBA-Exceptions wegen Verbindungsfehlern geworfen werden
+	@Override
+	public void exit() {
+		new Thread(new Runnable(){
+
+			@Override
+			public void run() {
+				orb.shutdown(true);
+			}
+			
+		}).start();
+	}
+}
